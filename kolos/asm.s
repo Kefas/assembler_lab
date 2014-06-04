6b71ee684ca3f42dc9644736d4efa66ff5a49686
Piotr Konsek <piotr@ThinkPad-Edge-E530.(none)>
2014-06-04 22:38:27 +0200

    up

--- kolos/asm.s
+++ kolos/asm.s
@@ -17,20 +17,15 @@ myprintf:
 	movl %esp, %ebp
 
 
-	movl 8(%ebp), %ecx
-	movl $0, %edi
-	SHL $8, %ecx
-	movb %ch,  rest(,%edi,1)	
+	movl 8(%ebp), %ecx	#kopiujemy 4bajty (bo movL) z adresu ebp + 8 do ecx
+	movl $0, %edi		#ustawiamy nasz 'indeks' na 0
+	SHL $8, %ecx		#przesuwamy w lewo nasz rejestr ecx
+	movb %ch,  rest(,%edi,1)#dzieki czemu z rejestru cl wszystko przeszlo do ch
+				#nie jest to konieczne
+				# tylko pokazuj ze sie da ;)
 	movl $0x04, %eax
 	movl $0x01, %ebx
-
-	#----
-	#movl $2, %edi
-	#movb $49, rest(,%edi,1) 
-	movl $rest, %ecx
-	#movb $'1', %cl
-	#---
-	
+	movl $rest, %ecx	
 	movl $4, %edx
 	
 	int  $0x80
